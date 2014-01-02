f3972bc3b6656b1e4603ed319c7aa00b5be657d0
lb71104208 <lb71104208@163.com>
2014-01-02 23:55:47 +0800

    ...

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -23,18 +23,29 @@ CCScene* GameScene::scene()
 {
     CCScene *scene = CCScene::create();
     CCNodeLoaderLibrary *lib = CCNodeLoaderLibrary::newDefaultCCNodeLoaderLibrary();
-    lib->registerCCNodeLoader("GameScene", MainSceneLayerLoader::loader());
+    lib->registerCCNodeLoader("GameScene", GameSceneLayerLoader::loader());
     CCBReader *reader = new CCBReader(lib);
-    CCNode *  gameScene = reader->readNodeGraphFromFile("stage-single.ccbi",scene);
+    CCNode *  gameScene = reader->readNodeGraphFromFile("stage-single.ccbi");
     reader->autorelease();
     
     if (gameScene!=NULL)
     {
         scene->addChild(gameScene); //将node 添加到scene中
     }
-    CCDirector::sharedDirector()->pushScene(scene);
-  //  CCDirector::sharedDirector()->getScheduler()->scheduleUpdateForTarget(this,0,false);
+    CCDirector::sharedDirector()->replaceScene(scene);
+    CCDirector::sharedDirector()->getScheduler()->scheduleUpdateForTarget(this,0,false);
     return scene;
+}
+
+SEL_MenuHandler GameScene::onResolveCCBCCMenuItemSelector(cocos2d::CCObject *pTarget, const char *pSelectorName)
+{
+    CCB_SELECTORRESOLVER_CCMENUITEM_GLUE(this, "pause", GameScene::pause);
+    return NULL;
+}
+
+SEL_CCControlHandler GameScene::onResolveCCBCCControlSelector(cocos2d::CCObject *pTarget, const char *pSelectorName)
+{
+    return NULL;
 }
 
 bool GameScene::onAssignCCBMemberVariable(cocos2d::CCObject *pTarget, const char *pMemberVariableName, cocos2d::CCNode *pNode)
@@ -48,9 +59,20 @@ bool GameScene::onAssignCCBCustomProperty(cocos2d::CCObject *pTarget, const char
     return false;
 }
 
+void GameScene::onNodeLoaded(CCNode * pNode, CCNodeLoader * pNodeLoader)
+{
+    CCLog("节点全部加载完毕");
+}
+
 void GameScene::update(float delta)
 {
     if (mBrickSprite!=NULL) {
         mBrickSprite->setPosition(CCPoint(mBrickSprite->getPosition().x+1,mBrickSprite->getPosition().y));
     }
+}
+
+void GameScene::pause(CCObject* pSender, CCControlEvent pCCControlEvent)
+
+{
+    CCLog("paused!");
 }
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -16,18 +16,33 @@ using namespace cocos2d;
 
 class GameScene:
 public CCLayer,
-public CCBMemberVariableAssigner{
+public CCBMemberVariableAssigner,
+public CCNodeLoaderListener ,
+public CCBSelectorResolver{
     
 public:
     CCScene* scene();
     GameScene();
+    CCB_STATIC_NEW_AUTORELEASE_OBJECT_WITH_INIT_METHOD(GameScene, create);
     virtual ~GameScene();
     virtual bool onAssignCCBMemberVariable(cocos2d::CCObject *pTarget, const char *pMemberVariableName, cocos2d::CCNode *pNode);
     virtual bool onAssignCCBCustomProperty(CCObject* pTarget, const char* pMemberVariableName, CCBValue* pCCBValue);
-
+     virtual SEL_MenuHandler onResolveCCBCCMenuItemSelector(CCObject* pTarget, const char *pSelectorName);
+    virtual SEL_CCControlHandler onResolveCCBCCControlSelector(cocos2d::CCObject * pTarget, const char* pSelectorName);
+    //节点加载
+    virtual void onNodeLoaded(CCNode * pNode, CCNodeLoader * pNodeLoader);
+    void pause(CCObject* pSender, CCControlEvent pCCControlEvent);
     void update(float dt);
 private:
     CCSprite* mBrickSprite;
+};
+
+class GameSceneLayerLoader:public CCLayerLoader  {
+    
+public:
+    CCB_STATIC_NEW_AUTORELEASE_OBJECT_METHOD(GameSceneLayerLoader, loader);
+protected:
+    CCB_VIRTUAL_NEW_AUTORELEASE_CREATECCNODE_METHOD(GameScene);
 };
 
 #endif /* defined(__LittleDemon__GameScene__) */
